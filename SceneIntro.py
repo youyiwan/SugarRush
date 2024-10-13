0f41f1c63d995a5784c61cc7805ab24c98949e16
youyiwan <youyiwan@gmail.com>
2024-10-13 13:32:07 +0800

    Modify width to 800 and Create 'Start Game' button.

--- SceneIntro.py
+++ SceneIntro.py
@@ -7,15 +7,20 @@ class SceneIntro(pyghelpers.Scene):
     def __init__(self, MY_WINDOW):
         self.MY_WINDOW = MY_WINDOW
 
+        self.startButtonText = pygwidgets.TextButton(MY_WINDOW, (325, 0),
+                                                     'Start Game', fontSize=35,
+                                                     overColor=RED, downColor=LIME)
+
     def getSceneKey(self):
         return SCENE_INTRO
 
 
     def handleInputs(self, eventsList, keyPressedList):
         for event in eventsList:
-            if 1==1:
-                print('TEST')
+            if self.startButtonText.handleEvent(event):
+                self.goToScene(SCENE_GAME)
 
     def draw(self):
         self.MY_WINDOW.fill(BLUE)
+        self.startButtonText.draw()
 
