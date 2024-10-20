1e701472e28b33d1338c4f0bc9e299e5450172b5
youyiwan <youyiwan@gmail.com>
2024-10-20 09:26:57 +0800

    Implement player piece to move along the board.

--- SceneGame.py
+++ SceneGame.py
@@ -22,6 +22,12 @@ class SceneGame(pyghelpers.Scene):
         self.counter = 0
         self.playerScore = 0
 
+        self.playerbutton = pygwidgets.CustomButton(self.MY_WINDOW, (0, 700),
+                                                    up='images/player.png',
+                                                    down='images/player.png',
+                                                    over='images/player.png')
+
+
 
     def fill_EvenBoard(self):
         i = 0
@@ -69,11 +75,29 @@ class SceneGame(pyghelpers.Scene):
 
     def handleInputs(self, eventsList, keyPressedList):
         for event in eventsList:
-            if 1==1:
-                self.goToScene(SCENE_GAME)
+            if self.playerbutton.handleEvent(event):
+                if 0 <= self.counter < 7 or 15 < self.counter < 23 or 31 < self.counter < 39 or 48 <= self.counter < 55:
+                    self.playerbutton.moveX(100)
+                    print(self.playerbutton.getLoc())
+                    self.counter = self.counter + 1
+                    print(self.counter)
+                elif 8 <= self.counter <= 14 or 23 < self.counter < 31 or 39 < self.counter < 47 or 55 < self.counter < 63:
+                    self.playerbutton.moveX(-100)
+                    self.counter = self.counter + 1
+                    print(self.playerbutton.getLoc())
+                    print(self.counter)
+                elif self.counter == 7 or self.counter == 15 or self.counter == 23 or self.counter == 31 or self.counter == 39 or self.counter == 47 or self.counter == 55:
+                    self.playerbutton.moveY(-100)
+                    self.counter = self.counter + 1
+                    print(self.playerbutton.getLoc())
+                    print(self.counter)
+                elif self.counter >= 63:
+                    print(self.playerbutton.getLoc())
 
     def draw(self):
         self.MY_WINDOW.fill(BLUE)
         draw_cubes(MY_WINDOW)
         self.fill_OddBoard()
         self.fill_EvenBoard()
+        self.playerbutton.draw()
+
